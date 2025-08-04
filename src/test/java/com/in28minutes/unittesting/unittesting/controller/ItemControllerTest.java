c4bb2b394548fa4e90a4b2de5a2d156860bd5132
Vignesh <vigneshamu007@gmail.com>
2025-08-04 13:39:41 +0530

    Update ItemControllerTest.java

--- src/test/java/com/in28minutes/unittesting/unittesting/controller/ItemControllerTest.java
+++ src/test/java/com/in28minutes/unittesting/unittesting/controller/ItemControllerTest.java
@@ -7,7 +7,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
-import org.skyscreamer.jsonassert;
+import org.skyscreamer.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
